22c0747112d9625bba5da66578adc454294217eb
jingchenlucky <251360270@qq.com>
2020-03-26 19:31:43 +0800

    1-3

--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-<h1>efre<span>1</span><span>222</span></h1>,
+<h1>efre<span>1</span><span>2233333333332</span></h1>,
 document.getElementById('root')
 );
