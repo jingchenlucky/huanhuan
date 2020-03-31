c4402036a935df4f1a0e9c505c18aff209955b03
jingchenlucky <251360270@qq.com>
2020-03-31 12:48:42 +0800

    2-2图片切换

--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -1,16 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import src1 from './assets/1.jpg';
+import src2 from './assets/2.jpg';
+import src3 from './assets/3.jpg';
+import './index.css';
 
-const num=1;
-const div1 = (
-   <div title="234445">
-       {num}
-   </div>
-)
-console.log(div1);
-// div1.props.title="666";//这里会报错
-// console.log(div1);
-ReactDOM.render(
-    div1,
-    document.getElementById('root')
-);
+const srcs = [src1, src2, src3];
+
+let index = 2;
+
+let timer;
+
+const container = document.getElementById('root');
+
+//渲染页面
+function render() {
+    ReactDOM.render(
+        <img src={srcs[index]}/>,
+        container
+    );
+}
+
+// 根据index来切换图片
+function start() {
+    stop();
+    timer = setInterval(() => {
+        console.log(index);
+        index = (index + 1) % 3;
+        render();
+    }, 1000)
+
+}
+
+function stop() {
+    clearInterval(timer);
+}
+
+render();
+start();
+// 鼠标移入定时器停止，鼠标移除定时器开始
+container.onmouseenter = function () {
+    stop();
+}
+container.onmouseleave = function () {
+    start();
+}
+
