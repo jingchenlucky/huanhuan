02ff74266191332677fc33c9037f046b37010675
jingchenlucky <251360270@qq.com>
2020-04-02 17:15:14 +0800

    2-5组件状态

--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import StudentList from './components/StudentList'
+import Tick from './components/Tick';
 
-const appkey = "huanhuan_1564064301241";
+let num = 10;
 
-// 获取学生数据接口
-async function fetchAllStudents() {
-    const stus = await fetch("http://open.duyiedu.com/api/student/findAll?appkey=" + appkey)
-        .then(resp => resp.json()).then(resp=>resp.data);
-    return stus;
-}
-
-async function render(){
-    ReactDOM.render("正在加载中",document.getElementById('root'));
-    const stus=await fetchAllStudents();
-    ReactDOM.render(<StudentList stus={stus}/>,document.getElementById('root'));
-}
-render();
+ReactDOM.render(<Tick number={10}/>, document.getElementById('root'))
