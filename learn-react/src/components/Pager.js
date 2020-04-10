ab478c0c07a7ac3b5694b6c070818081cfaac93d
jingchenlucky <251360270@qq.com>
2020-04-10 08:05:33 +0800

    2-8分页组件

---
+++ learn-react/src/components/Pager.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Pager extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Pager;
--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TickControl from './TickControl';
 
 ReactDOM.render(<TickControl/>, document.getElementById('root'))
 
