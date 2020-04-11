d2f83ac823f1040cff1c24601bf5ec2a2707e807
jingchenlucky <251360270@qq.com>
2020-04-11 11:12:51 +0800

    2-8分页插件

--- learn-react/src/components/Pager.js
+++ learn-react/src/components/Pager.js
@@ -1,13 +1,36 @@
-import React, {Component} from 'react';
+import React from 'react';
+import './Pager.css';
 
-class Pager extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+/**
+ * 分页组件
+ * 属性：
+ * 1. current：初始页码
+ * 2. total：总数据量
+ * 3. limit：页容量，每页显示的数据量
+ * 4. panelNumber：数字页码最多显示多少个
+ * 5. onPageChange：当页码改变的事件
+ */
+function Pager(props) {
+    const pageNumber=getPagerNumer(props);
+    console.log(pageNumber);
+    return (
+        <div>
+            <span
+                onClick={()=>{
+                    if(props.current==1){
+                        return;
+                    }
+                    props.onPageChange&&props.onPageChange(1);
+                }}
+                className={props.current===1?'item disabled':'item'}>首页</span>
+            <span className={props.current===1?'item disabled':'item'}>上一页</span>
+            <span className={props.current===pageNumber?'item disabled':'item'}>下一页</span>
+            <span className={props.current===pageNumber?'item disabled':'item'}>尾页</span>
+        </div>
+    );
+}
+function getPagerNumer(props) {
+    return Math.ceil(props.total/props.limit);
 }
 
 export default Pager;
--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Pager from './components/Pager';
 
-ReactDOM.render(<TickControl/>, document.getElementById('root'))
+ReactDOM.render(<Pager
+    current={12}
+    total={100}
+    limit={9}
+    panelNumber={5}
+/>, document.getElementById('root'))
 
