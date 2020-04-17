365c82af82388d1fed7475323e94ff2f239ca734
jingchenlucky <251360270@qq.com>
2020-04-17 14:07:58 +0800

    2-10生命周期

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -2,18 +2,28 @@ import React, {Component} from 'react';
 import OldLifeCycle from './OldLifeCycle'
 
 class App extends Component {
-    state={
-        number:1
+    state = {
+        number: 1,
+        show: true
     }
+
     render() {
+        const comp = this.state.show ? <OldLifeCycle n={this.state.number}/> : null;
         return (
             <div>
-                <OldLifeCycle n={this.state.number}/>
-                <button onClick={()=>{
-                    this.setState(state=>({
-                        number:state.number+1
+                {comp}
+                <button onClick={() => {
+                    this.setState(state => ({
+                        number: state.number + 1
                     }))
-                }}>n加1</button>
+                }}>n加1
+                </button>
+                <button onClick={() => {
+                    this.setState(state => ({
+                        show: !this.state.show
+                    }))
+                }}>显示隐藏
+                </button>
             </div>
         );
     }
