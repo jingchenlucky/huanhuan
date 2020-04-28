293957c3d32b3aa69172ed1ead9eb08c2399f490
jingchenlucky <251360270@qq.com>
2020-04-28 20:43:06 +0800

    2-11传递元素内容

--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import Comp from './Comp'
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+// ReactDOM.render(<Comp html={<p>123456</p>}/>, document.getElementById('root'))
+ReactDOM.render(
+    <Comp content2={<h2>第二组元素内容</h2>} content3={<h2>第3组元素内容</h2>}>
+        <p>123456789</p>
+    </Comp>,
+    document.getElementById('root'))
 
