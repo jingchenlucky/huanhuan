ae160bd4fcbc51f647f1bb6d0fb447833ef85e50
jingchenlucky <251360270@qq.com>
2020-04-28 21:22:25 +0800

    2-6自动移动的小球

---
+++ learn-react/src/components/common/Modal/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './index.css'
+
+class Index extends Component {
+    render() {
+        return (
+            <div className="modal">
+
+            </div>
+        );
+    }
+}
+
+export default Index;
