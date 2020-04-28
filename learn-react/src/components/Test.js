34ce4aa14a308c9ae36b5ae05d9c371c3f8d47d2
jingchenlucky <251360270@qq.com>
2020-04-28 22:10:16 +0800

    2-12制作蒙层组件

--- learn-react/src/components/Test.js
+++ learn-react/src/components/Test.js
@@ -2,14 +2,34 @@ import React, {Component} from 'react';
 import Modal from './common/Modal';
 
 class Test extends Component {
+    state = {
+        showModal: false
+    }
+    showModal=()=>{
+        this.setState({showModal: true})
+    }
+    hideModal=()=>{
+        this.setState({showModal: false})
+    }
+
     render() {
         return (
             <div>
-                <img src="https://www.baidu.com/link?url=RJFzgRT-ZPPnQwWPrZ7O19aLyXr44iZc2eDyC37vUgc0cGNt98jjUUQp-_L0BeHir1XqTm8-zcZqOEhFdMJWtGGnT0fhNFFHEH1tA7ocW_1OJ_DBz-8myZGwlACnxq7y7gDvXceH43HjKB75dEKIHOGQ_qmfq8MUCJhdN2AFovE6KlGjnpWDdg4W9RE5KaDjV35kR6RsWdKDc9QSEvsmf-xrn2ThNMDTVQyD8K54UIitcQJlJMBr9G2yvlsRtKSSd0Ccr-cFcV7Yq6wiTZ74nEG-BxtPWq2qmg8SgWQxZZOUSj0R4EJUuNA7AOtfsNX9CLoODyVvhECxir_f-w2UepsYbPL_dR6Ntcp9_Gpof37M2Zn5iSXODloGXPctg-jGaKf-4QzxHKkXshffe3CFqwkiuoiJq6VgVS4qGJe-ebya9pQ3e2lHFGjdJ48KHISODEcxOYQ7Z90XA-r0fhi7DzsC-WLBUY3amMRkXbcbK-2QrqKiu4KXwWrJ9Z6RPPaoFQA9XpJm9aPlTn4alus4sHYXxYhF4miAe5zao4oWGYn_VQV1T2MjUOchSg2HaWCs396QTf9VLjejXJmoKawvVuIRpXv3XLc--QbogI_1927&timg=https%3A%2F%2Fss0.bdstatic.com%2F94oJfD_bAAcT8t7mm9GUKT-xh_%2Ftimg%3Fimage%26quality%3D100%26size%3Db4000_4000%26sec%3D1588079966%26di%3Dcaedcb1774c200652f4d95046f4edbc3%26src%3Dhttp%3A%2F%2Fa3.att.hudong.com%2F14%2F75%2F01300000164186121366756803686.jpg&click_t=1588079970532&s_info=1440_821&wd=&eqid=bc98d56d00019603000000035ea82d5e" alt=""/>
-                <Modal ></Modal>
+                <img
+                    src="https://dss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3252521864,872614242&fm=26&gp=0.jpg"
+                    alt=""/>
+                {this.state.showModal ?
+                    (<Modal bg="rgba(200,200,100,0.5)" onClose={this.hideModal}>
+                        <h1>21324453534535254</h1>
+                        <button onClick={this.hideModal}>关闭梦层
+                        </button>
+                    </Modal>) : null}
+                <button onClick={this.showModal}>显示梦层
+                </button>
             </div>
         );
     }
 }
 
 export default Test;
+
--- learn-react/src/components/common/Modal/index.js
+++ learn-react/src/components/common/Modal/index.js
@@ -1,14 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './index.css'
 
-class Index extends Component {
-    render() {
-        return (
-            <div className="modal">
-
-            </div>
-        );
+function Index(props) {
+    console.log(props);
+    var defaultProps = {
+        bg: 'rgba(0,0,0,0.5)'
     }
+    var datas = Object.assign({}, defaultProps, props)
+    return (
+        <div className="modal" style={{background: datas.bg}} onClick={e=>{
+            if(e.target.className==='modal'){
+                datas.onClose();
+            }
+        }}>
+            <div className="modal-center">
+                {props.children}
+            </div>
+
+        </div>
+    );
 }
 
 export default Index;
