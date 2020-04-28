1535912686899b1451a70ca01784ed75519691ec
jingchenlucky <251360270@qq.com>
2020-04-28 21:09:02 +0800

    2-6自动移动的小球

---
+++ learn-react/src/components/Test.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Modal from './common/Modal';
+
+class Test extends Component {
+    render() {
+        return (
+            <div>
+                <Modal></Modal>
+            </div>
+        );
+    }
+}
+
+export default Test;
