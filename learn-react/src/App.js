3b57b475831fbb0f2431c5020ef6fda146ac049d
jingchenlucky <251360270@qq.com>
2020-08-11 20:58:17 +0800

    Context Hook

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,34 +1,9 @@
 import React from 'react';
-import useReducer from './useReducer';
 
-function reducer(state, action) {
-    switch (action.type) {
-        case 'increase':
-            return state + 2;
-        case "decrease":
-            if (state === 0) {
-                return 0;
-            }
-            return state - 2;
-        default:
-            return state;
-    }
-}
-
-export default function App(props) {
-    const [n, dispatch] = useReducer(reducer, 0);
-
+function App(props) {
     return (
-        <div>
-            <button onClick={() => {
-                dispatch({type: 'decrease'})
-            }}>-
-            </button>
-            <span>{n}</span>
-            <button onClick={() => {
-                dispatch({type: 'increase'})
-            }}>+
-            </button>
-        </div>
+        <div></div>
     );
 }
+
+export default App;
