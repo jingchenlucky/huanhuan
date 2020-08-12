19f4571a438cfc51e0e428a3d5ade8300cee0778
jingchenlucky <251360270@qq.com>
2020-08-12 08:23:20 +0800

    Memo Hook

--- learn-react/src/App.copy01.js
+++ learn-react/src/App.copy01.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react';
+import React, {useMemo, useState} from 'react';
 
 class Test extends React.PureComponent {
     render() {
@@ -14,16 +14,13 @@ function Parent() {
     const [txt, setTxt] = useState(123);
     const [n, setN] = useState(0);
     console.log("Parent Render");
-    // useCallback解决了因为函数地址变化导致的问题，如果依赖项没有发生变化，赋值地址就不会变化。如果依赖项发生了变化，赋值为新的地址
-    const handleClick = useCallback(() => {
-        setTxt(123);
-    }, [])
+    const handleClick = useMemo(() => {
+        return () => {
+            setTxt(txt + 1);
+        }
+    }, [txt])
     return (
         <div>
-            {/*函数的地址每次渲染发生了变化，导致子组件跟着重新渲染，若子组件是经过优化的组件，则可能会导致优化失效*/}
-            {/*<Test text={txt} onClick={() => {*/}
-            {/*    setTxt(123)*/}
-            {/*}}/>*/}
             <Test text={txt} onClick={handleClick}/>
             <input type="number" value={n} onChange={e => {
                 setN(parseInt(e.target.value))
--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,42 +1,31 @@
-import React, {useCallback, useState} from 'react';
+import React, {useMemo, useState} from 'react';
 
-class Test extends React.PureComponent {
-    render() {
-        console.log("Test Render");
-        return <div>
-            <h1>{this.props.text}</h1>
-            <button onClick={this.props.onClick}>改变文本</button>
-        </div>
-    }
-}
-
-function Parent() {
-    const [txt, setTxt] = useState(123);
-    const [n, setN] = useState(0);
-    console.log("Parent Render");
-    // useCallback解决了因为函数地址变化导致的问题，如果依赖项没有发生变化，赋值地址就不会变化。如果依赖项发生了变化，赋值为新的地址
-    const handleClick = useCallback(() => {
-        setTxt(123);
-    }, [])
-    return (
-        <div>
-            {/*函数的地址每次渲染发生了变化，导致子组件跟着重新渲染，若子组件是经过优化的组件，则可能会导致优化失效*/}
-            {/*<Test text={txt} onClick={() => {*/}
-            {/*    setTxt(123)*/}
-            {/*}}/>*/}
-            <Test text={txt} onClick={handleClick}/>
-            <input type="number" value={n} onChange={e => {
-                setN(parseInt(e.target.value))
-            }}/>
-        </div>
-    )
+function Item(props) {
+    return <li>{props.value}</li>
 }
 
 export default function App() {
+    const [range] = useState({min: 1, max: 1000});
+    const [n, setN] = useState(0);
+    // const list = [];
+    // for (let i = range.min; i < range.max; i++) {
+    //     console.log(i);
+    //     list.push(<Item key={i} value={i}></Item>)
+    // }
+    const list = useMemo(() => {//list数组没有发生变化，且数组中每一项都没发生变化，不会重新渲染
+        const list = [];
+        for (let i = range.min; i < range.max; i++) {
+            console.log(i);
+            list.push(<Item key={i} value={i}></Item>)
+        }
+        return list
+    }, [range.max, range.min]);
     return (
         <div>
-            <Parent/>
+            <ul>{list}</ul>
+            <input type="number" value={n} onChange={e => setN(parseInt(e.target.value))}/>
         </div>
+
 
     );
 }
