7955ffa31f8426e9a83083ef9e191a0c7b0bbb01
jingchenlucky <251360270@qq.com>
2020-08-15 12:45:24 +0800

    动画2

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,37 +1,21 @@
 import React, {useState} from 'react';
-import {Transition} from 'react-transition-group';
+import {CSSTransition} from 'react-transition-group';
 import './App.css';
 
-const duration = 2000;
-
-// const defaultStyle = {
-//     transition: `opacity ${duration}ms ease-in-out`,
-//     opacity: 0,
-// }
-//
-// const transitionStyles = {
-//     entering: {opacity: 1},
-//     entered: {opacity: 1},
-//     exiting: {opacity: 0},
-//     exited: {opacity: 0},
-// };
-
-export default function App() {
-    const [inProp, setInProp] = useState(false);
+export default function App(props) {
+    const [visible, setVisible] = useState(false);
     return (
         <div>
-            <Transition in={inProp} timeout={duration} onEnter={()=>{console.log("Enter之前");}}>
-            {state => {
-            console.log(state);
-            return <div className={state}>
-            I'm a fade Transition!
-            </div>
-        }}
-        </Transition>
-    <button onClick={() => setInProp(!inProp)}>
-        Click to Enter
-    </button>
-</div>
-)
-    ;
+            <CSSTransition
+                appear
+                classNames="test" timeout={2000} in={visible}>
+                <h1>一个标题</h1>
+            </CSSTransition>
+            <button onClick={() => {
+                setVisible(!visible)
+            }}>按钮
+            </button>
+        </div>
+    );
 }
+
