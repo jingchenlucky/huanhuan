a8d04cb506fb37eb8f286112195fdad5fe13ce96
jingchenlucky <251360270@qq.com>
2020-08-20 08:04:25 +0800

    5-1 react-router

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,43 +1,7 @@
-import React, {Component} from 'react';
-import FadeTransition from './components/common/FadeTransition';
-import {TransitionGroup} from "react-transition-group";
-import {v4 as uuidv4} from 'uuid';
+import React from 'react';
 
-export default class App extends Component {
-    state = {
-        taskList: [{id: uuidv4(), name: '任务1'}, {id: uuidv4(), name: '任务2'}, {id: uuidv4(), name: '任务3'}]
-    }
-
-    render() {
-        return (
-            <div>
-                <TransitionGroup component="ul">
-                    {this.state.taskList.map(it => (
-                        <FadeTransition appear classNames="abc" key={it.id} timeout={500}>
-                            <li>{it.name}
-                                <button onClick={() => {
-                                    this.setState({
-                                        taskList: this.state.taskList.filter(item => (item.id !== it.id))
-                                    })
-
-                                }}>删除
-                                </button>
-                            </li>
-                        </FadeTransition>
-                    ))}
-
-                </TransitionGroup>
-
-                <button onClick={() => {
-                    var name = window.prompt("请输入任务名称")
-                    this.setState({
-                        taskList: [...this.state.taskList, {id: uuidv4(), name}]
-                    })
-
-                }}>
-                    添加按钮
-                </button>
-            </div>
-        );
-    }
+export default function App(props) {
+    return (
+        <div></div>
+    );
 }
