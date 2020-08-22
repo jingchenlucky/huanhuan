0e5ea397b3c1817d595d16c05cb455af517adf57
jingchenlucky <251360270@qq.com>
2020-08-22 19:13:58 +0800

    5-5路由信息

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,14 +1,51 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Login from './pages/Login'
-import Admin from './pages/Admin'
+import {BrowserRouter as Router, Route, Switch, withRouter} from 'react-router-dom';
+
+const AWrapper = withRouter(A);
+// function withRouter(Comp) {
+//     return function routerWrapper(props) {
+//         //获取上下文中的信息
+//         return <Comp {...props} history={上下文中的history} />
+//     }
+// }
+function News(props) {
+    return (
+        <>
+            <div>新闻列表</div>
+            {/*<A/>*/}
+            <AWrapper/>
+        </>
+    )
+}
+
+function A(props) {
+    console.log(props);
+    return (<button onClick={() => {
+        props.history.push("/")
+    }}>
+        点击返回
+    </button>)
+}
+
+function Index() {
+    return (
+        <div>首页</div>
+    )
+}
+
+function notFound() {
+    return (
+        <div>找不到页面。。</div>
+    )
+}
 
 export default function App(props) {
     return (
         <Router>
             <Switch>
-                <Route path='/login' exact component={Login}/>
-                <Route path='/' component={Admin}/>
+                <Route path="/news" component={News}/>
+                <Route path="/" component={Index}/>
+                <Route component={notFound}/>
             </Switch>
         </Router>
     );
