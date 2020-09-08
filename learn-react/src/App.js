0710650719e56824b00b73ccfea02b9291e7a07a
jingchenlucky <251360270@qq.com>
2020-09-08 21:57:28 +0800

    导航守卫

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 import RouteGuard from './RouteGuard';
 
 function Page1() {
@@ -12,22 +12,34 @@ function Page2() {
 
 
 export default function App(props) {
+    let count=0;
     return (
+        <RouteGuard
+            onBeforeChange={(prevLoaction, location, action, callback, unBlock) => {
+                // console.log("想要改变");
+                console.log(`页面想要从${prevLoaction.pathname}跳转到${location.pathname}，跳转方式是${action}，允许跳转`)
+                callback(true);
+                unBlock();//取消阻塞，仅阻塞了一次
+            }}
+            onChange={(prevLocation, location, action, unListen) => {
+                console.log(`监听器日志：${count++}从地址${prevLocation}变化为${location.pathname},变化方式${action}`);
+                if (count === 5) {
+                    unListen();
+                }
+            }}
+        >
+            <ul>
+                <li>
+                    <Link to="/page1">页面1</Link>
+                </li>
+                <li>
+                    <Link to="/page2">页面2</Link>
+                </li>
+            </ul>
 
-        <Router>
-            {/*<ul>*/}
-            {/*    <li>*/}
-            {/*        <Link to="/page1">页面1</Link>*/}
-            {/*    </li>*/}
-            {/*    <li>*/}
-            {/*        <Link to="/page2">页面2</Link>*/}
-            {/*    </li>*/}
-            {/*</ul>*/}
-            <RouteGuard>
-                <Route path='/page1' component={Page1}></Route>
-                <Route path='/page2' component={Page2}></Route>
-            </RouteGuard>
-        </Router>
+            <Route path='/page1' component={Page1}></Route>
+            <Route path='/page2' component={Page2}></Route>
+        </RouteGuard>
 
 
     );
--- learn-react/src/RouteGuard.js
+++ learn-react/src/RouteGuard.js
@@ -1,15 +1,62 @@
 import React, {Component} from 'react';
-import {withRouter} from 'react-router-dom';
+import {withRouter, BrowserRouter as Router} from 'react-router-dom';
+
+let prevLoaction, location, action, unBlock;
+
+class _GuardHelper extends Component {
+    componentDidMount() {
+        unBlock = this.props.history.block((newlocation, ac) => {
+            prevLoaction = this.props.location;//之前地址
+            location = newlocation;//新地址
+            action = ac;
+            // console.log(this.props.location);
+            // console.log(location);
+            // console.log(action);
+            return '23425';
+        });
+        //添加监听器
+        this.unListen = this.props.history.listen((location, action) => {
+            // console.log(location);
+            // console.log("地址变化了");
+            // console.log(action);
+            this.props.onChange && this.props.onChange(this.props.location.pathname, location, action, this.unListen);
+        })
+    }
+    componentWillUnmount() {
+        unBlock();
+        //卸载监听器
+        this.unListen();
+    }
+
+    render() {
+        return null;
+    }
+}
+
+const GuardHelper = withRouter(_GuardHelper);
 
 class RouteGuard extends Component {
+
+    handleConfirm = (msg, callback) => {
+        // console.log(msg);
+        if (this.props.onBeforeChange) {
+            this.props.onBeforeChange(prevLoaction, location, action, callback,unBlock);
+        }else{
+            callback(true);
+        }
+    }
+
     render() {
-        console.log(this.props);
         return (
             <div>
-                {this.props.children}
+                <Router getUserConfirmation={this.handleConfirm}>
+                    <GuardHelper onChange={this.props.onChange}/>
+                    {this.props.children}
+                </Router>
+
             </div>
         );
     }
 }
 
-export default withRouter(RouteGuard);
+export default RouteGuard;
