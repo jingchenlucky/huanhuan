2c817790462ffc73c8a22035de75c435fcf945e1
jingchenlucky <251360270@qq.com>
2020-09-11 07:26:40 +0800

    滚动条复位

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import * as Pages from './Page';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+// import * as Pages from './Page';
+// import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css'
 
 function App(props) {
     return (
         <div className="main">
-            <Router>
-                <Pages.NavBar></Pages.NavBar>
-                <div className="page-container">
-                    <Route path="/" component={Pages.Home} exact/>
-                    <Route path="/news" component={Pages.News} exact/>
-                    <Route path="/personal" component={Pages.Personal} exact/>
-                </div>
+            {/*<Router>*/}
+            {/*    <Pages.NavBar></Pages.NavBar>*/}
+            {/*    <div className="page-container">*/}
+            {/*        <Route path="/" component={Pages.Home} exact/>*/}
+            {/*        <Route path="/news" component={Pages.News} exact/>*/}
+            {/*        <Route path="/personal" component={Pages.Personal} exact/>*/}
+            {/*    </div>*/}
 
-            </Router>
+            {/*</Router>*/}
         </div>
     );
 }
