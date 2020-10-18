9613b4975afe511a170cac855dc6e3a1076e6863
jingchenlucky <251360270@qq.com>
2020-10-18 15:14:41 +0800

    7-7bandActionCreator

--- learn-react/src/store/index.js
+++ learn-react/src/store/index.js
@@ -1,26 +1,27 @@
-// import {createStore} from 'redux';
-import { createStore } from "../redux";
+// import { createStore, bindActionCreators } from "redux";
+import { createStore, bindActionCreators } from "../redux";
 import reducer from "./reducer";
 import { createAddUserAction, deleteUserAction } from "./action/usersAction";
-// import { v4 as uuidv4 } from 'uuid';
 
 // const store = createStore(reducer,10);
 const store = createStore(reducer);
 console.log("仓库store", store);
 
-// const unlisten = 
 store.subscribe(() => {
-  console.log("状态发生改变了11,在分发过action之后运行");
+  console.log("状态发生改变了11,在分发过action之后运行", store.getState());
 });
-store.subscribe(() => {
-  console.log(store.getState());
-});
-store.dispatch(
-  createAddUserAction({
-    id: 3,
-    name: "用户3333",
-    age: 8,
-  })
-);
-// unlisten();
-store.dispatch(deleteUserAction(3));
+// store.dispatch(
+//   createAddUserAction({
+//     id: 3,
+//     name: "用户3333",
+//     age: 8,
+//   })
+// );
+// store.dispatch(deleteUserAction(3));
+const actionCreators = {
+  addUser: createAddUserAction,
+  deleteUser: deleteUserAction,
+};
+const actions = bindActionCreators(actionCreators, store.dispatch);
+actions.addUser({ id: 3, name: "用户3333", age: 8 });
+actions.deleteUser(3);
