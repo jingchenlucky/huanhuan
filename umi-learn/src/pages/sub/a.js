a57250164e1af5647e334a24c92a47493c602135
jingchenlucky <251360270@qq.com>
2021-03-24 08:57:53 +0800

    学生管理系统

--- umi-learn/src/pages/sub/a.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function a() {
-    return (
-        <div>
-            sub/a页面
-        </div>
-    )
-}
--- umi-learn/src/pages/sub/sub/b.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function b() {
-    return (
-        <div>
-            sub/sub/b页面
-        </div>
-    )
-}
