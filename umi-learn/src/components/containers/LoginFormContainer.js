95bca752867b64aae133a22249763f7972621c81
jingchenlucky <251360270@qq.com>
2021-03-30 07:57:09 +0800

    学生管理

--- umi-learn/src/components/containers/LoginFormContainer.js
+++ umi-learn/src/components/containers/LoginFormContainer.js
@@ -1,10 +1,20 @@
 import { connect } from 'dva';
+import { routerRedux } from 'dva/router';
 
 import LoginForm from '../LoginForm';
 const mapDispatchToProps = dispatch => ({
-  onLogin(loginId, loginPwd) {
-    dispatch({ type: 'loginUser/login', payload: { loginId, loginPwd } });
+  async onLogin(loginId, loginPwd) {
+    //dispatch 触发异步 返回回来是一个promise
+    const result = await dispatch({
+      type: 'loginUser/login',
+      payload: { loginId, loginPwd },
+    });
+    if (result) {
+      dispatch(routerRedux.push('/'));
+    } else {
+      alert('账号密码错误');
+    }
   },
 });
 
-export default connect(null,mapDispatchToProps)(LoginForm);
+export default connect(null, mapDispatchToProps)(LoginForm);
--- umi-learn/src/models/loginUser.js
+++ umi-learn/src/models/loginUser.js
@@ -1,3 +1,5 @@
+// import { history } from 'umi';
+import { routerRedux } from 'dva/router';
 export default {
   state: null,
   reducers: {
@@ -11,6 +13,15 @@ export default {
       if (loginId == 'admin' && loginPwd == '123456') {
         yield put({ type: 'setLoginUser', payload: loginId });
         localStorage.setItem('loginId', loginId);
+        //1可以在请求接口这里统一处理
+        // 页面跳转的两种方式
+        // history.push('/')
+        // yield put(routerRedux.push('/'));//分发一个action的方式页面跳转
+        //2.也可以在LoginFormContainer中对接口返回回来对结果处理
+        return true;
+      } else {
+        // alert('账号密码错误');
+        return false;
       }
     },
     *loginOut(action, { put }) {
