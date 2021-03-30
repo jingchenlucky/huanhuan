30bb1c900d9bb0f0d9b373621a9871207dcc0f83
jingchenlucky <251360270@qq.com>
2021-03-30 09:01:57 +0800

    2342

---
+++ umi-learn/src/routes/PrivateRouter.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function PrivateRouter(props) {
+    console.log("++++++++++",props)
+    return (
+        <div>
+            
+        </div>
+    )
+}
