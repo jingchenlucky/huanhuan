8029f095bf771a3c86808de69e1fbd1a0cf497d8
jingchenlucky <251360270@qq.com>
2021-07-04 14:15:42 +0800

    约定路由

--- umi-learn/src/layouts/index.js
+++ umi-learn/src/layouts/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'umi'; //实际上就是react-router-dom中的NavLink组件
 import WithRouterA from '@/components/A'; //在react-router-dom中需要用withRouter包裹 嵌套的组件才能拿到路由信息 history等  umi也是。
+//如果要在 普通组建而不是页面组建中使用路由信息，要用WithRouterA包裹
 import Menu from '@/components/Menu'; //但是Menu是通过倒入的history进行跳转，不用包裹
 
 export default function index(props) {
