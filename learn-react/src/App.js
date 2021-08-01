ef58de8b98618861a8d6509efd34f9b91073869f
jingchenlucky <251360270@qq.com>
2021-08-01 15:31:43 +0800

    history

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 // import * as Pages from './Page';
-// import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import matchPath from './react-router/matchPath';
 
 function App(props) {
-  const result=  matchPath('/news/:year?/:month?/:day','/news/2021/08/20',{exact:true});
-  console.log("========",result)
+  // const result=  matchPath('/news/:year?/:month?/:day','/news/2021/08/20',{exact:true});
+  // console.log("========",result)
     return (
-        <div>123231</div>
+        <Router></Router>
     );
 }
 
